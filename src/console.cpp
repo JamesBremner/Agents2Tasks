0f2f827735c8289e03c015e8158b3e8575facb0a
JamesBremner <james@ravenspoint.com>
2023-11-08 17:22:11 -0500

    Add console app #6

---
+++ src/console.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "allocator.h"
+
+main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cout << "Usage: Agents2TasksConsole (input file path) ( output file path )";
+        exit(1);
+    }
+
+    cAllocator A;
+    A.readfile(argv[1]);
+    A.maxflow();
+    A.hungarian();
+    A.agents2tasks();
+    A.writefile(argv[2]);
+
+    return 0;
+}
