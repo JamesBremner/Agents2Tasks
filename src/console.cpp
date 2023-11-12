8dc4b83e21960ba43b8923953561c1448ed8da88
JamesBremner <james@ravenspoint.com>
2023-11-12 14:15:56 -0500

    Basic error handling for console app #12

--- src/console.cpp
+++ src/console.cpp
@@ -1,20 +1,42 @@
 #include <iostream>
+#include <fstream>
 #include "allocator.h"
+
+void errorHandler(
+    const std::string &fname,
+    const std::string &msg)
+{
+    std::ofstream ofs(fname);
+    if (!ofs.is_open())
+        std::cout << "Cannot open output file\n";
+    ofs << msg << "\n";
+    ofs.close();
+    std::cout << msg << "\n";
+    exit(2);
+}
 
 main(int argc, char *argv[])
 {
     if (argc != 3)
-    {
-        std::cout << "Usage: Agents2TasksConsole (input file path) ( output file path )";
-        exit(1);
-    }
+        errorHandler(
+            "Agents2TasksConsole.txt",
+            "Usage: Agents2TasksConsole (input file path) ( output file path )");
 
-    cAllocator A;
-    readfile(A,argv[1]);
-    A.maxflow();
-    A.hungarian();
-    Janusz(A);
-    writefile( A, argv[2] );
+    try
+    {
+        cAllocator A;
+        readfile(A, argv[1]);
+        A.maxflow();
+        A.hungarian();
+        Janusz(A);
+        writefile(A, argv[2]);
+    }
+    catch (std::exception &e)
+    {
+        errorHandler(
+            argv[2],
+            e.what());
+    }
 
     return 0;
 }
